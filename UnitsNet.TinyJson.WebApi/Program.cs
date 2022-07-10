98863f0c2ddd1e9928ca3e0d851a04c608cdcb59
Don Waldo <don.g.waldo@outlook.com>
2022-07-09 22:26:53 -0500

    Adds swagger mapping for schema

--- UnitsNet.TinyJson.WebApi/Program.cs
+++ UnitsNet.TinyJson.WebApi/Program.cs
@@ -1,17 +1,22 @@
-using System.ComponentModel;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers().AddNewtonsoftJson(jsonOptions =>
 {
     jsonOptions.SerializerSettings.Converters.Add(new UnitsNetTinyJsonConverter());
+    jsonOptions.SerializerSettings.Converters.Add(new StringEnumConverter(typeof(CamelCaseNamingStrategy)));
 });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c => {
+    c.MapType<Length>(() => new OpenApiSchema { Type = "string", Example = new OpenApiString("10|in") });
+});
 
 var app = builder.Build();
 
@@ -29,18 +34,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-//JsonConvert.DefaultSettings = () => new JsonSerializerSettings()
-//{
-//    Converters = new List<JsonConverter> { new UnitsNetTinyJsonConverter() }
-//};
-
-TypeDescriptor.AddAttributes(typeof(Length), new TypeConverterAttribute(typeof(UnitsNetTinyTypeConvert<Length>)));
-//TypeDescriptor.AddAttributes(typeof(IEnumerable<Length>), new TypeConverterAttribute(typeof(UnitsNetTinyTypeConvert<IEnumerable<Length>>)));
-
-
-//public void DoMagic()
-//{
-//    // NOTE: After this, you can use your typeconverter.
-//    AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
-//}
